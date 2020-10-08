2cb1706147996021a9f135ac0f13b50e1ca7f114
GeorgeDoit <aelgdd@gmail.com>
2020-10-08 13:49:47 +0300

    step 3

--- src/codehub/bootcamp/assignment1/Main.java
+++ src/codehub/bootcamp/assignment1/Main.java
@@ -2,14 +2,21 @@ package codehub.bootcamp.assignment1;
 
 import codehub.bootcamp.assignment1.model.Customer;
 import codehub.bootcamp.assignment1.model.OnlineCustomer;
+import codehub.bootcamp.assignment1.model.Product;
 import codehub.bootcamp.assignment1.tester.Tester;
 
 public class Main {
 
     public static void main(String[] args) {
 	// write your code here!
-        Tester t = new Tester();
-        t.TestCode();
+//        Tester t = new Tester();
+//        t.TestCode();
 
+        Product p1 = new Product("mouse", 22.1, 23.1);
+        Product p2 = new Product("mouse2", 33.1, 34.1);
+
+        System.out.println("product with id: " + p1.getId() + " availability is " + p1.getAvailability());
+        p2.setAvailability(false);
+        System.out.println("product with id: " + p2.getId() + " availability is " + p2.getAvailability());
     }
 }
---
+++ src/codehub/bootcamp/assignment1/model/Product.java
@@ -0,0 +1,56 @@
+package codehub.bootcamp.assignment1.model;
+
+public class Product {
+    private int id;
+    private String name;
+    private double priceWhenBuy;
+    private double priceWhenSell;
+    private boolean availability;
+
+    static int counter = 1;
+
+    public Product(String name, double priceWhenBuy, double priceWhenSell) {
+        this.id = counter ++;
+        this.name = name;
+        this.priceWhenBuy = priceWhenBuy;
+        this.priceWhenSell = priceWhenSell;
+        this.availability = true;
+
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public void setPriceWhenBuy(double priceWhenBuy) {
+        this.priceWhenBuy = priceWhenBuy;
+    }
+
+    public void setPriceWhenSell(double priceWhenSell) {
+        this.priceWhenSell = priceWhenSell;
+    }
+
+    public void setAvailability(boolean x){
+        this.availability = x;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public double getPriceWhenBuy() {
+        return priceWhenBuy;
+    }
+
+    public double getPriceWhenSell() {
+        return priceWhenSell;
+    }
+
+    public boolean getAvailability(){
+        return availability;
+    }
+}
